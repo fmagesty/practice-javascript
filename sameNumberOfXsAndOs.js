0a81abb9fc18f0b0a42cd986eb1b98bb52027d44
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-02-22 20:13:36 -0300

    Add files via upload

---
+++ sameNumberOfXsAndOs.js
@@ -0,0 +1,28 @@
+// Test to see if string has the same numbers of 'x' and 'o'. Return true if positive and false if negative.
+
+function XO(str) {
+    let x = 0;
+    let o = 0;
+    for (i in str) {
+    if (str[i].toLowerCase() === 'x') {
+        x += 1;
+    }
+    }
+    for (j in str) {
+    if (str[j].toLowerCase() === 'o'){
+        o += 1;
+    }
+    }
+    if (x === o) {
+    return true;
+    }
+    else {
+    return false;
+    }
+}
+
+console.log(XO('xo'));
+console.log(XO("xxOo"));
+console.log(XO("xxxm"));
+console.log(XO("Oo"));
+console.log(XO("ooom"));
