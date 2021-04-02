6cf1fe7d39850f7072fc04e73094738f84a0cb29
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-04-02 17:41:38 -0300

    Add files via upload

---
+++ reduce.js
@@ -0,0 +1,7 @@
+// Just practing how to use the reduce method in JS.
+
+array = [2, 3, 7, 10];
+
+const somaReduce = array.reduce((valorAcumulado, numeroDoArray) => valorAcumulado * numeroDoArray, 1)
+
+console.log(somaReduce)
