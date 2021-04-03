cd5c6d3f11bc5ea822f84de42edd8933db075533
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-04-03 16:34:13 -0300

    Add files via upload

--- reduce.js
+++ reduce.js
@@ -7,38 +7,38 @@ const multiplicacaoReduce = numerosArr.reduce((valorAcumulado, numeroDoArray) =>
 console.log(multiplicacaoReduce)
 
 const pessoas = [
-      {
+    {
         nome: 'Ayrton',
         idade: 27
-      },
-      {
+    },
+    {
         nome: 'João',
         idade: 14
-      },
-      {
+    },
+    {
         nome: 'Maria',
         idade: 23
-      },
-      {
+    },
+    {
         nome: 'Joana',
         idade: 21
-      },
-      {
+    },
+    {
         nome: 'Lucas',
         idade: 32
-      },
-      {
+    },
+    {
         nome: 'Mateus',
         idade: 15
-      },
-      {
+    },
+    {
         nome: 'Isa',
         idade: 23
-      },
-      {
+    },
+    {
         nome: 'Luiza',
         idade: 17
-      }
+    }
     ];
 
 const pessoasAgrupadas = pessoas.reduce(function(valorAcumulador, valorArray) {
