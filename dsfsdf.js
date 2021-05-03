947b65b1244edbbf042dbe645169da08c993c7a8
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-05-03 13:08:30 -0300

    deletion of test

--- dsfsdf.js
+++
@@ -1,9 +0,0 @@
-function testPalavra(letras) {
-  contador = "";
-  for (let i = 0; i < letras.length; i++) {
-    contador += letras[i];
-    console.log(contador);
-  }
-}
-
-testPalavra("aabbcc");
